e8e8090a69ab9540b831e4480788d70c97703bf0
p-diogo.fonteles <p-diogo.fonteles@pd.tec.br>
2024-05-24 13:11:00 -0300

    chore: config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseService } from './infra/database/DatabaseConnection';
+import { SignupController } from './auth/controllers/signup.controller';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
+import { AuthModule } from './auth/auth.module';
+import { SigninController } from './auth/controllers/signin.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (config) => envSchema.parse(config),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [SignupController, SigninController],
+  providers: [DatabaseService],
 })
 export class AppModule {}
